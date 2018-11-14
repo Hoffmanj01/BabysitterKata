bb46daf53c31facf0ce2e5d0feec868b213eb6d9
HoffmanJ01 <HoffmanJ01@gmail.com>
2018-11-14 18:20:24 -0500

    Added the GetInvoicePrice Method framework

---
+++ BabysitterKata/BabysitterKata.Tests/InvoiceControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BabysitterKata.Tests
+{
+    public class InvoiceControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- BabysitterKata/BabysitterKata.Web/Controllers/InvoiceController.cs
+++ BabysitterKata/BabysitterKata.Web/Controllers/InvoiceController.cs
@@ -9,6 +9,14 @@ namespace BabysitterKata.Web.Controllers
     [ApiController]
     public class InvoiceController : ControllerBase
     {
-        
+        [Route("[controller]/GetInvoicePrice")]
+        [HttpGet]
+        public IActionResult GetInvoicePrice(DateTime StartDate, DateTime BedTime, DateTime EndTime)
+        {
+            //The plan is to make the most money while not over exagurating the hours due to the change in hourly wages
+            double result = 0;
+
+            return Ok(result);
+        }
     }
 }
