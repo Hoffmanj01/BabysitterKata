5eb0635de01ac3b7a544ec3df6c2f5f688c71bd9
HoffmanJ01 <HoffmanJ01@gmail.com>
2018-11-14 18:10:02 -0500

    Initial Commit

---
+++ BabysitterKata/BabysitterKata.Web/Controllers/InvoiceController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabysitterKata.Web.Controllers
+{
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        
+    }
+}
---
+++ BabysitterKata/BabysitterKata.Web/Controllers/TimeTrackingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabysitterKata.Web.Controllers
+{
+    public class TimeTrackingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
