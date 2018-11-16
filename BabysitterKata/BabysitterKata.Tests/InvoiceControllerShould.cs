f80cf6b978bf91a84bd97567cef6cf921be31edd
HoffmanJ01 <HoffmanJ01@gmail.com>
2018-11-15 19:32:30 -0500

    made changes to the UI to reqire datetimes over just times fixed folder issue

--- BabysitterKata/BabysitterKata.Tests/InvoiceControllerShould.cs
+++ BabysitterKata/BabysitterKata.Tests/InvoiceControllerShould.cs
@@ -52,6 +52,19 @@ namespace BabysitterKata.Tests
             Assert.True(124 == value);
         }
 
+        [Fact]
+        public void ReturnTheCorrectValuesWithIncorrectDates()
+        {
+            DateTime startTime = new DateTime(2018, 11, 15, 17, 00, 00);
+            DateTime bedTime = new DateTime(2018, 11, 15, 18, 00, 00);
+            DateTime endTime = new DateTime(2018, 11, 15, 4, 00, 00);
+
+            OkObjectResult result = _sut.GetInvoicePrice(startTime, bedTime, endTime) as OkObjectResult;
+
+            double? value = result.Value as double?;
+            Assert.True(124 == value);
+        }
+
         //partial hours
         //partial hours after midnight
 
@@ -59,6 +72,8 @@ namespace BabysitterKata.Tests
 
         //leaving before bedtime but after midnight
 
+        //daylight savings time
 
+        //mimimulistic time
     }
 }
